9ac661ccf33c07a28178d4216936969da73c86d7
limsubinn <mymyx02@naver.com>
2025-01-31 20:40:10 +0900

    [Refactor] 상품 목록 조회 로직 수정
    - 쿼리 1개로 수정
    - 정렬 기본 조건 설정

--- BE/src/main/java/com/d201/fundingift/product/service/ProductService.java
+++ BE/src/main/java/com/d201/fundingift/product/service/ProductService.java
@@ -12,7 +12,6 @@ import com.d201.fundingift.product.entity.Product;
 import com.d201.fundingift.product.repository.ProductCategoryRepository;
 import com.d201.fundingift.product.repository.ProductOptionRepository;
 import com.d201.fundingift.product.repository.ProductRepository;
-import com.d201.fundingift.wishlist.entity.Wishlist;
 import com.d201.fundingift.wishlist.repository.WishlistRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -41,41 +40,47 @@ public class ProductService {
     private final FundingRepository fundingRepository;
     private final SecurityUtil securityUtil;
 
-    // 카테고리 리스트 조회
+    /** 상품 카테고리 목록 조회 **/
     public List<GetProductCategoryResponse> getCategories() {
         return productCategoryRepository.findAllByDeletedAtIsNull()
                 .stream().map(GetProductCategoryResponse::from)
                 .collect(Collectors.toList());
     }
 
-    // 상품 리스트 조회
+    /** 상품 목록 조회 **/
     public SliceList<GetProductResponse> getProducts(Integer categoryId, String keyword, Integer page, Integer size, Integer sort) {
-        // 페이징 객체
-        Pageable pageable = PageRequest.of(page, size, getSort(sort));
-
-        /**
-         * 전체 목록 조회
-         */
-        if (categoryId == null) {
-            // 키워드 X
-            if (keyword == null) {
-                return getProductResponseSliceList(productRepository.findAllSlice(pageable));
-            }
-            // 키워드 O
-            return getProductResponseSliceList(productRepository.findAllSliceByKeyword(keyword, pageable));
+        if (categoryId != null) {
+            validateCategoryId(categoryId);
         }
 
-        /**
-         * 카테고리 별 목록 조회
-         */
-        validateCategoryId(categoryId); // 카테고리 유효성 검사
+        Slice<Product> products = productRepository.findAllSliceByCategoryIdAndKeyword(categoryId, keyword, PageRequest.of(page, size, getSort(sort)));
+        return getProductResponseSliceList(products);
+    }
 
-        // 키워드 X
-        if (keyword == null) {
-            return getProductResponseSliceList(productRepository.findAllSliceByCategoryId(categoryId, pageable));
+    private Sort getSort(Integer sort) {
+        Sort defaultSort = Sort.by("id").descending();
+        if (sort == 0) { // 기본 (최신 순)
+            return defaultSort;
         }
-        // 키워드 O
-        return getProductResponseSliceList(productRepository.findAllSliceByCategoryIdAndKeyword(categoryId, keyword, pageable));
+        if (sort == 1) { // 리뷰 많은 순
+            return Sort.by("reviewCnt").descending().and(defaultSort);
+        }
+        if (sort == 2) { // 평점 높은 순
+            return Sort.by("reviewAvg").descending().and(defaultSort);
+        }
+        if (sort == 3) { // 가격 높은 순
+            return Sort.by("price").descending().and(defaultSort);
+        }
+        if (sort == 4) { // 가격 낮은 순
+            return Sort.by("price").ascending().and(defaultSort);
+        }
+        throw new CustomException(SORT_NOT_FOUND);
+    }
+
+    private SliceList<GetProductResponse> getProductResponseSliceList(Slice<Product> products) {
+        return SliceList.from(products.stream().map(GetProductResponse::from).collect(Collectors.toList()),
+                products.getPageable(),
+                products.hasNext());
     }
 
     public SliceList<GetProductResponse> getProductsRank(Integer page, Integer size) {
@@ -93,38 +98,6 @@ public class ProductService {
         boolean isWishlist = getIsWishlist(productId);
         // 반환
         return GetProductDetailResponse.from(product, options, isWishlist);
-    }
-
-    // 정렬 객체
-    private Sort getSort(Integer sort) {
-        // 기본 순 (최신 순)
-        if (sort == 0) {
-            return Sort.by("id").descending();
-        }
-        // 리뷰 많은 순
-        if (sort == 1) {
-            return Sort.by("reviewCnt").descending();
-        }
-        // 평점 높은 순
-        if (sort == 2) {
-            return Sort.by("reviewAvg").descending();
-        }
-        // 가격 높은 순
-        if (sort == 3) {
-            return Sort.by("price").descending();
-        }
-        // 가격 낮은 순
-        if (sort == 4) {
-            return Sort.by("price").ascending();
-        }
-        // 예외
-        throw new CustomException(SORT_NOT_FOUND);
-    }
-
-    private SliceList<GetProductResponse> getProductResponseSliceList(Slice<Product> products) {
-        return SliceList.from(products.stream().map(GetProductResponse::from).collect(Collectors.toList()),
-                products.getPageable(),
-                products.hasNext());
     }
 
     private List<GetProductOptionResponse> getOptions(Product product) {
